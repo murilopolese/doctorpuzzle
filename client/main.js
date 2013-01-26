14188fd9d2b3154aac8afa4c0acfc2076a8aaba3
Murilo Polese <murilopolese@gmail.com>
2013-01-26 10:29:10 -0200

    Funções iniciais de gerar e renderizar board

--- client/main.js
+++ client/main.js
@@ -1,6 +1,36 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+var board = new Array();
+var elements = [
+    {
+        element_id: 'red'
+    }, 
+    {
+        element_id: 'green'
+    }, 
+    {
+        element_id: 'blue'
+    }, 
+];
 
+Meteor.startup(function() {
+    console.log('iniciou appplicação');
+    populateBoard(4, 4);
+    drawBoard();
+});
 
+function populateBoard(width, height) {
+    for(i = 0; i < width; i++) {
+        board[i] = [];
+        for(j = 0; j < height; j++) {
+            board[i][j] = 
+                elements[parseInt(Math.random()*elements.length)].element_id
+        }
+    }
+}
+
+function drawBoard() {
+    for(i = 0; i < board.length; i++) {
+        for(j = 0; j < board[i].length; j++) {
+            $('#board').append('<div class="piece ' + board[i][j] + '"></div>');
+        }
+    }
+}
--- server/main.js
+++ server/main.js
@@ -1,6 +1,4 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+Meteor.startup(function() {
+    console.log('Iniciou Servidor');
+    
+})
