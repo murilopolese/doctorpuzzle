9cae3b5c0a4e0ec579e94cf72421392474b8e4ed
Murilo Polese <murilopolese@gmail.com>
2013-01-26 11:02:54 -0200

    Fincção para verificar se dois blocos são vizinhos

--- client/main.js
+++ client/main.js
@@ -1,20 +1,30 @@
 var board = new Array();
 var elements = [
-    {
-        element_id: 'red'
-    }, 
-    {
-        element_id: 'green'
-    }, 
-    {
-        element_id: 'blue'
-    }, 
+{
+    element_id: 'red'
+}, 
+{
+    element_id: 'green'
+}, 
+{
+    element_id: 'blue'
+} 
 ];
+var selectedPiece;
 
 Meteor.startup(function() {
     console.log('iniciou appplicação');
     populateBoard(4, 4);
     drawBoard();
+    
+    $('.piece').click(function() {
+        if(selectedPiece == undefined) {
+            selectedPiece = $(this).attr('id');
+            console.log(selectedPiece);
+        } else if(isNear($(this).attr('id'))) {
+            console.log('is near')
+        }
+    });
 });
 
 function populateBoard(width, height) {
@@ -22,15 +32,32 @@ function populateBoard(width, height) {
         board[i] = [];
         for(j = 0; j < height; j++) {
             board[i][j] = 
-                elements[parseInt(Math.random()*elements.length)].element_id
+            elements[parseInt(Math.random()*elements.length)].element_id
         }
     }
 }
 
 function drawBoard() {
+    $('#board').html('');
     for(i = 0; i < board.length; i++) {
         for(j = 0; j < board[i].length; j++) {
-            $('#board').append('<div class="piece ' + board[i][j] + '"></div>');
+            $('#board').append('<div id="' + i + 'x' + j + '" class="piece ' + board[i][j] + '"></div>');
         }
     }
+}
+
+function isNear(id) {
+    var selectedCoord = selectedPiece.split('x');
+    var clickedCoord = id.split('x');
+    
+    if((clickedCoord[0] == selectedCoord[0]) &&
+    (Math.abs(clickedCoord[1] - selectedCoord[1]) == 1)) {
+        return true;
+    }
+    if((clickedCoord[1] == selectedCoord[1]) &&
+    (Math.abs(clickedCoord[0] - selectedCoord[0]) == 1)) {
+        return true;
+    }
+    
+    return false;
 }
